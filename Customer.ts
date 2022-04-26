ec50aad1bdfa4f8d21a798857bd5a8cde5a12de6
Ganesh Kumar <ganeshkumarmacherla@gmail.com>
2022-04-26 14:25:47 +0530

    TypeScript Assignment

---
+++ Customer.ts
@@ -0,0 +1,27 @@
+class Customer{
+
+   public cid: number = 0;
+   public cName: String = "";
+   public cLocation: String = "";
+
+   constructor(cid: number, cName: String = "", cLocation: String = "") {
+        this.cid = cid;
+        this.cName = cName;
+        this.cLocation = cLocation;
+   }
+
+   public showDetails(): void {
+       console.log("Customer Id "+this.cid+" Customer Name "+this.cName+" Customer Location "+this.cLocation);
+       console.log("--------------------------------------");
+   }
+}
+
+// let c1: Customer = new Customer();
+let c2: Customer = new Customer(101);
+let c3: Customer = new Customer(102, "Ganesh");
+let c4: Customer = new Customer(103, "Macherla", "MannarPolur");
+
+// c1.showDetails();
+c2.showDetails();
+c3.showDetails();
+c4.showDetails();
