3f614a28b434287bcc27fc46bb6297fe96ab3e4d
Ganesh Kumar <ganeshkumarmacherla@gmail.com>
2022-04-26 15:16:50 +0530

    tasks

---
+++ TypeScript/Day1/Task2.ts
@@ -0,0 +1,8 @@
+let employeeId : number = 248;
+let employeeName : string = "Ganesh";
+let employeeTitle : string = "Team Lead";
+let isPermanentEmp : boolean = true;
+let empSalary : number = 95000.00;
+let empDeptNo : number = 11;
+
+console.log(`Employee Id : ${employeeId} \nEmployee Name : ${employeeName}\nEmployee Job : ${employeeTitle}\nIs Permanent Employee : ${isPermanentEmp}\nEmployee Salary : ${empSalary}\nEmployee Dept Number : ${empDeptNo} `);
