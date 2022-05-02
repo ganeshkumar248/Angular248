8385660eb324034661157101f09447948eebd4ca
Ganesh Kumar <52152897+ganeshkumar248@users.noreply.github.com>
2022-05-02 15:04:55 +0530

    Directives task

---
+++ Angular/Day4/Assignment1/product-list/product-list.component.ts
@@ -0,0 +1,54 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../models/product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+
+  // @Input()
+  // category: string = "";
+ 
+  // productFilter:Product[]= [];
+
+  // start: number = 0;
+  // end: number = 4;
+  // isDisabled: boolean = false;
+
+  displayMode: string =  "Table";
+
+  productArray: Product[] = [
+    {id: 101, name: "Redmi", price: 25000, category: "Mobile", qty: 1, picture: "assets/images/phone-image.png"},
+    {id: 102, name: "LG", price: 15000, category: "Mobile", qty: 2, picture: "assets/images/phone-image.png"},
+    {id: 103, name: "Samsung", price: 35000, category: "Mobile", qty: 3, picture: "assets/images/phone-image.png"},
+    {id: 104, name: "Poco", price: 17500, category: "Mobile", qty: 4, picture: "assets/images/phone-image.png"},
+    {id: 201, name: "Dell", price: 55000, category: "Laptop", qty: 1, picture: "assets/images/laptop-image.png"},
+    {id: 202, name: "Lenovo", price: 65500, category: "Laptop", qty: 2, picture: "assets/images/laptop-image.png"},
+    {id: 203, name: "HP", price: 76500, category: "Laptop", qty: 3, picture: "assets/images/laptop-image.png"},
+    {id: 104, name: "Asus", price: 35000, category: "Laptop", qty: 4, picture: "assets/images/laptop-image.png"},
+    {id: 301, name: "Canon", price: 8500, category: "Camera", qty: 1, picture: "assets/images/camera-image.png"},
+    {id: 302, name: "Sony", price: 5000, category: "Camera", qty: 2, picture: "assets/images/camera-image.png"},
+    {id: 303, name: "Nikon", price: 9000, category: "Camera", qty: 3, picture: "assets/images/camera-image.png"},
+    {id: 304, name: "Samsung", price: 3500, category: "Camera", qty: 4, picture: "assets/images/camera-image.png"},
+  ]
+
+  constructor() { }
+
+  set_displayMode_List() {
+    this.displayMode = "List";
+  }
+
+  set_displayMode_Table() {
+    this.displayMode = "Table";
+  }
+
+  set_displayMode_small() {
+    this.displayMode = "smallImage";
+  }
+
+  set_displayMode_large() {
+    this.displayMode = "largeImage";
+  }
+}
---
+++ Angular/Day4/Assignment1/product.ts
@@ -0,0 +1,10 @@
+export class Product
+{
+    id: number = 0;
+    name: string = "";
+    price: number = 0;
+    category: string = "";
+    qty: number = 1;
+    picture: string ="";
+}
+
